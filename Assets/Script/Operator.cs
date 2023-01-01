b660a05a788fd6711b05aa6dc015a352216f136b
Usef Farahmand <50228817+JosephFarahmand@users.noreply.github.com>
2023-01-01 15:56:15 +0330

    feat create operator script

---
+++ Assets/Script/Operator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+
+public class Operator : MonoBehaviour
+{
+    [SerializeField] private Key m_Ctrl;
+    [SerializeField] private Key m_Key;
+
+    private bool m_HoldCtrl;
+    private bool m_HoldKey;
+
+    public event Action OnCallback;
+
+    private void OnEnable()
+    {
+        m_Ctrl.OnHold += HoldCtrl;
+        m_Key.OnHold += HoldKey;
+
+        m_Key.OnPress += Press;
+    }
+
+    private void OnDisable()
+    {
+        m_Ctrl.OnHold -= HoldCtrl;
+        m_Key.OnHold -= HoldKey;
+
+        m_Key.OnPress -= Press;
+    }
+
+    private void HoldCtrl(bool value)
+    {
+        m_HoldCtrl = value;
+    }
+
+    private void HoldKey(bool value)
+    {
+        m_HoldKey = value;
+    }
+
+    private void Press()
+    {
+        if (m_HoldCtrl && m_HoldKey)
+        {
+            OnCallback?.Invoke();
+        }
+    }
+}
